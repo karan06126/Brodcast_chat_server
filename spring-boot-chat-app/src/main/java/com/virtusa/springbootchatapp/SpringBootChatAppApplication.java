5e30736d2136cc1d6d712197d66120638f3f3c78
Devilal Godara <devilalgodara98@gmail.com>
2021-10-06 13:26:23 +0530

    Add files via upload

---
+++ spring-boot-chat-app/src/main/java/com/virtusa/springbootchatapp/SpringBootChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.springbootchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatAppApplication.class, args);
+	}
+
+}
